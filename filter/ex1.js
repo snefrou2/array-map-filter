1c82877000ffd7bde5316f330857245376915165
sophie <sneuville59@gmail.com>
2020-03-24 08:48:59 +0100

    MaJ exo

--- filter/ex1.js
+++ filter/ex1.js
@@ -13,7 +13,8 @@ Sortie attendue:
 function getPositiveNumbers(numbers) {
   return numbers.filter((numbers)=> numbers>=0)}
 
-    
+  // OK ne pas toucher  
+  
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node filter/ex1
  //console.log(getPositiveNumbers([-2, -1, 0, 1, 2]));
--- filter/ex3.js
+++ filter/ex3.js
@@ -28,11 +28,13 @@ Sortie attendue:
   ]
  */
 
+
 function filterOnPrice(products, maxPrice) {
  return products.filter((product) =>product.price<=maxPrice)
 }
 
 
+//OK ne pas toucher
 
 // Ne pas modifier l'export
 
